8ec88906708e9e450f38916a14cb8a87970a2de2
Jess Panni <jess.panni@endjin.com>
2015-11-30 08:26:03 +0000

    Moving powershell snippets into different folders based on version of Azure Powershell

---
+++ 1.0/Snippets.ps1
@@ -0,0 +1,28 @@
+﻿# To login to Azure Resource Manager
+Login-AzureRmAccount
+
+# You can also use a specific Tenant if you would like a faster login experience
+# Login-AzureRmAccount -TenantId xxxx
+
+# To view all subscriptions for your account
+Get-AzureRmSubscription
+
+# To select a default subscription for your current session
+Get-AzureRmSubscription –SubscriptionName “your sub” | Select-AzureRmSubscription
+
+# View your current Azure PowerShell session context
+# This session state is only applicable to the current session and will not affect other sessions
+Get-AzureRmContext
+
+# To select the default storage context for your current session
+Set-AzureRmCurrentStorageAccount –ResourceGroupName “your resource group” –StorageAccountName “your storage account name”
+
+# View your current Azure PowerShell session context
+# Note: the CurrentStoargeAccount is now set in your session context
+Get-AzureRmContext
+
+# To import the Azure.Storage data plane module (blob, queue, table)
+Import-Module Azure.Storage
+
+# To list all of the blobs in all of your containers in all of your accounts
+Get-AzureRmStorageAccount | Get-AzureStorageContainer | Get-AzureStorageBlob
